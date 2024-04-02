a2fc0c1b367c6f442d3b47290f47cded9d6a5b0b
José Cabeda <jecabeda@gmail.com>
2024-04-02 13:51:00 +0100

    Add support for the same output formats of edge tts

--- audiobook_generator/tts_providers/coqui_provider.py
+++ audiobook_generator/tts_providers/coqui_provider.py
@@ -2,6 +2,7 @@ import logging
 import math
 
 import torch
+from pydub import AudioSegment
 from TTS.api import TTS
 
 from audiobook_generator.config.general_config import GeneralConfig
@@ -16,15 +17,15 @@ class CoquiProvider(BaseTTSProvider):
     def __init__(self, config: GeneralConfig):
         # Init TTS with the target model name
 
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         logger.setLevel(config.log)
 
         # TTS provider specific config
-        config.output_format = config.output_format or "wav"
-        config.model_name = config.model_name or "tts_models/en/ljspeech/tacotron2-DDC"
-        config.language_coqui = config.language_coqui or "en"
-        config.voice_sample_wav_path = config.voice_sample_wav_path or ""
+        config.output_format = config.output_format or 'mp3'
+        config.model_name = config.model_name or 'tts_models/en/ljspeech/tacotron2-DDC'
+        config.language_coqui = config.language_coqui or 'en'
+        config.voice_sample_wav_path = config.voice_sample_wav_path or ''
 
         self.tts = TTS(
             model_name=config.model_name,
@@ -35,7 +36,7 @@ class CoquiProvider(BaseTTSProvider):
         super().__init__(config)
 
     def __str__(self) -> str:
-        return f"{self.config}"
+        return f'{self.config}'
 
     def validate_config(self):
         pass
@@ -46,21 +47,26 @@ class CoquiProvider(BaseTTSProvider):
         output_file: str,
         audio_tags: AudioTags,
     ):
+        wav_file_path = output_file[:-4] + '.wav'
+
         if self.tts.is_multi_lingual:
             print(len(text))
             self.tts.tts_to_file(
                 text,
                 speaker_wav=self.config.voice_sample_wav_path,
                 language=self.config.language_coqui,
-                file_path=output_file,
+                file_path=wav_file_path,
                 split_sentences=True,
             )
         else:
             self.tts.tts_to_file(
                 text,
-                file_path=output_file,
+                file_path=wav_file_path,
                 split_sentences=True,
             )
+
+        # Convert the wav file to the desired format
+        AudioSegment.from_wav(wav_file_path).export(output_file, format=self.config.output_format)
 
         set_audio_tags(output_file, audio_tags)
 
@@ -68,14 +74,28 @@ class CoquiProvider(BaseTTSProvider):
         return math.ceil(total_chars / 1000) * self.price
 
     def get_break_string(self):
-        return "    "
+        return '    '
 
     def get_output_file_extension(self):
-        if self.config.output_format.startswith("wav"):
-            return "wav"
+        if self.config.output_format.startswith('amr'):
+            return 'amr'
+        elif self.config.output_format.startswith('ogg'):
+            return 'ogg'
+        elif self.config.output_format.endswith('truesilk'):
+            return 'silk'
+        elif self.config.output_format.endswith('pcm'):
+            return 'pcm'
+        elif self.config.output_format.startswith('raw'):
+            return 'wav'
+        elif self.config.output_format.startswith('webm'):
+            return 'webm'
+        elif self.config.output_format.endswith('opus'):
+            return 'opus'
+        elif self.config.output_format.endswith('mp3'):
+            return 'mp3'
         else:
             raise NotImplementedError(
-                f"Unknown file extension for output format: {self.config.output_format}"
+                f'Unknown file extension for output format: {self.config.output_format}'
             )
 
     def get_supported_models(self):
